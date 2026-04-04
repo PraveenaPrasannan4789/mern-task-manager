6373e505de75f694878937196b90dcd95ec7d84b
Praveena Prasannan <49147219+PraveenaPrasannan4789@users.noreply.github.com>
2026-04-04 11:23:33 +0530

    added auth middleware and controllers

---
+++ server/controller/authController.js
@@ -0,0 +1,42 @@
+// controllers/authController.js
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const signup = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) return res.status(400).json({ msg: "Email already exists" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({ name, email, password: hashedPassword });
+    await user.save();
+
+    res.status(201).json({ msg: "User created" });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ msg: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
+
+    const token = jwt.sign({ userId: user._id, role: user.role }, "secret", { expiresIn: "1h" });
+
+    res.json({ token });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports={login,signup}
